9ade9403ed8bab4f878f915731b8403060eca83b
takuya <takuya.ikeda@tri-ad.global>
2020-10-08 10:06:53 +0900

    fix seed best tuned using texture4.jpeg

--- dib-render/train_cam_pos_optim.py
+++ dib-render/train_cam_pos_optim.py
@@ -42,6 +42,18 @@ WIDTH = 512  # 256
 import torch.nn.functional as F
 
 
+def set_seed(seed: int = 666):
+    """Set seed for reproducibility."""
+    np.random.seed(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+
+set_seed(777)
+
+
 def format_tensor(input,
                   dtype=torch.float32,
                   device: str = "cpu") -> torch.Tensor:
